903d730a6542f29cb37c1ade045023d952144c6d
adeltancsik <tancsikadel@gmail.com>
2019-12-06 14:34:52 +0100

    Showing answered questions and check if answers are correct or not

--- src/Components/TestResults/TestResults.js
+++ src/Components/TestResults/TestResults.js
@@ -3,12 +3,65 @@ import { connect } from "react-redux";
 
 class TestResults extends Component {
   render() {
+    const questionArray = this.props.history.map(submittedAnswer => {
+      return submittedAnswer[0].question;
+    });
+    const submittedAnswerIds = this.props.history.map(submittedAnswer => {
+      return submittedAnswer[0].history[0].answerId;
+    });
+
     return (
-      <ul>
-        {this.props.history.map(answer => {
-          return <li>{answer.answerId}</li>;
+      <ol>
+        {questionArray.map(question => {
+          return (
+            <li>
+              {question.questionContent}
+              <ul>
+                {this.props.history.map(submittedAnswer => {
+                  return submittedAnswer
+                    .filter(answer => {
+                      return answer.questionId === question.id;
+                    })
+                    .map(answer => {
+                      if (
+                        submittedAnswerIds.includes(answer.id) &&
+                        answer.correct
+                      ) {
+                        return (
+                          <li style={{ color: "green" }}>
+                            {answer.answerContent}
+                          </li>
+                        );
+                      } else if (
+                        submittedAnswerIds.includes(answer.id) &&
+                        !answer.correct
+                      ) {
+                        return (
+                          <li
+                            style={{
+                              color: "red",
+                              textDecoration: "line-through"
+                            }}
+                          >
+                            {answer.answerContent}
+                          </li>
+                        );
+                      } else if (answer.correct) {
+                        return (
+                          <li style={{ color: "green" }}>
+                            {answer.answerContent}
+                          </li>
+                        );
+                      } else {
+                        return <li>{answer.answerContent}</li>;
+                      }
+                    });
+                })}
+              </ul>
+            </li>
+          );
         })}
-      </ul>
+      </ol>
     );
   }
 }
